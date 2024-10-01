ec97b927674493ee2bcd22fb6d65baf812c58848
tjhoan <toro.jhoan@correounivalle.edu.co>
2024-09-30 23:42:23 -0500

    :rocket: Proyecto Creado

---
+++ lib/features/authentication/screens/onboarding.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+            // Páginas desplazables horizontalmente
+            PageView(
+              children: [
+                Column(
+                  // Image(image: AssetImage(TImage.onBoardingImage1),)
+                )
+            
+
+
+
+              ],
+            ),
+
+
+            // botón de omitir
+
+            // navegador de puntos smoothpageindicator
+
+            // botón circular
+        ],
+      ),
+    );
+  }
+}
+
